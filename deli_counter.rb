cc45dda87f643ac35006e86c2675fed17bce5202
Joyce Chun <wooyangee@gmail.com>
2018-04-22 05:32:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,7 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(name)
+  if katz_deli == 0 
+    
