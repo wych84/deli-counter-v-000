0702d06028384e82e9ed0cbe6cf8aa6a2aa8b5c9
Joyce Chun <wooyangee@gmail.com>
2018-04-22 04:49:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   current_line = []
   if array.count == 0 
     puts "The line is currently empty."
-  elsif array.count >= 1 
+  else
     array.each_with_index {|number, name|
     current_line << "#{number}. #{name}"
   }
