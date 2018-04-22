ab1c2ec079622bc33eefb0eda75dc8b70450f771
Joyce Chun <wooyangee@gmail.com>
2018-04-22 05:02:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,5 @@
 def line(katz_deli)
   if katz_deli.count !=0 
-    line_position = katz_deli.each_with_index {|number, person| puts "#{number + 1}. #{person} "}
-    puts "The line is currently: #{line_position}"
-  else
-    puts "The line is currently empty."
-  end 
+    
 end
 
