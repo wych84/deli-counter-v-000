cec387559827b69d2771df1f45bf1f2410187f29
Joyce Chun <wooyangee@gmail.com>
2018-04-22 05:37:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(name)
-  if katz_deli.count == 0
+  if katz_deli.count = 0
     puts "There is nobody waiting to be served."
   else
     puts "Currently serving #{katz_deli.shift}."
