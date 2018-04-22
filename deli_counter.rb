72f7da43a6c0edadd2a0590c495b60c025078ce0
Joyce Chun <wooyangee@gmail.com>
2018-04-22 05:05:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@ def line(katz_deli)
   current_line = []
   if katz_deli.count !=0 
     current_line.each_with_index {|index, name| 
-    current_line.push()}
+    current_line.push("#{index + 1}. #{name}")}
+    
 end
 
