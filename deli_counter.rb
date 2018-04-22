026e1130849aa0ab43f9095a805dfc9ed0b58664
Joyce Chun <wooyangee@gmail.com>
2018-04-22 04:54:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,4 @@
 def line(array)
-  current_line = []
-  if array.empty? 
-    puts "The line is currently empty."
-  else
-    array.each_with_index {|number, name|
-    current_line << "#{number.to_i + 1}. #{name}"
-  }
-   puts "The line is currently: #{current_line.join(" ")}"
-  end
+
 end
 
