ad7f8f1491b6cc6d1f95d0c3075e3953625e2550
Joyce Chun <wooyangee@gmail.com>
2018-04-22 05:06:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ def line(katz_deli)
     current_line.each_with_index {|index, name| 
     current_line.push("#{index + 1}. #{name}")}
   end
+  puts "The line is currently:#{current_line.join("")}"
+  else
   puts 
 end
 
