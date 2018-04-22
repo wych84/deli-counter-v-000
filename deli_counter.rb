464da2ae1f9f8f3c0a450d8d76b6efc22a50801b
Joyce Chun <wooyangee@gmail.com>
2018-04-22 04:44:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
-def line(katz_deli)
-  if katz_deli.count == 0 
-    puts ""
+def line(array)
+  if array.count == 0 
+    puts "The line is currently empty."
+  elsif array.count >= 1 
   katz_deli.each_with_index {|number, name|
-  puts "The line is currently: #{number}. #{name}"
+    puts "The line is currently: #{number}. #{name}"
   }
+  end 
+end
