9ca9b06586407247fd1e688514af3251e54a2d34
Joyce Chun <wooyangee@gmail.com>
2018-04-22 05:45:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(name)
+def now_serving(katz_deli)
   if katz_deli.empty?
     message = "There is nobody waiting to be served!"
   else
