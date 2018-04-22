02607d261c4ad83ffc8d6c68c8df8c4ce16737d0
Joyce Chun <wooyangee@gmail.com>
2018-04-22 05:45:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,10 +17,9 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
-    message = "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
-    message = "Currently serving #{katz_deli[0]}."
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   end
-puts message  
 end
